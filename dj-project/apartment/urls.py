5bd8a6c4c7746bdfd621f254a87d30b555d6e1d2
Yousef Sedik <joemohmmed00@gmail.com>
2024-02-12 00:04:13 +0200

    Add/Delete/Edit Apartment Media

--- dj-project/apartment/urls.py
+++ dj-project/apartment/urls.py
@@ -7,12 +7,16 @@ urlpatterns = [
     path('add/', views.AddApartmentView.as_view(), name='add-apartment'), 
     path('edit/<int:pk>', views.EditApartmentView.as_view(), name='edit-apartment'), 
     path('delete/<int:pk>',views.delete_apartment, name='delete-apartment'), 
+    path('AddMedia/<int:pk>', views.add_media_page, name='add_media_page'),
     
 ]
 
 
 htmx_urlpatterns = [
     path('reversePublicity/<int:pk>', views.reverse_public_visibility, name='reversePublicity'),
+    path('add-media/<int:pk>', views.add_media, name='add-media' ),
+    path('delete-media/<int:pk>', views.delete_media, name='delete_media'),
+    
 ]
 
 urlpatterns += htmx_urlpatterns
--- dj-project/apartment/views.py
+++ dj-project/apartment/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404, Http404
 from django.http.response import HttpResponse
 from django.views.generic import TemplateView, ListView, CreateView, UpdateView, DetailView
-# from django.views.generic.edit import
 from django.db.models import OuterRef, Subquery
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Apartment, Photo, Video
@@ -15,9 +14,17 @@ class ApartmentView(LoginRequiredMixin, ListView):
     model = Apartment 
     template_name = 'apartments/home.html'
     context_object_name = 'apartment_list' 
+
     def get_queryset(self):
         queryset = super(ApartmentView, self).get_queryset()
+        queryset = queryset.filter(owner=self.request.user)
+        queryset = queryset.annotate(
+            first_photo=Subquery(
+                Photo.objects.filter(apartment_id=OuterRef('pk')).order_by('pk').values('photo')[:1]
+            )
+        )
         return queryset
+
 class ViewApartmentView(DetailView):
     model = Apartment 
     template_name = 'apartments/view_apart.html'
@@ -30,6 +37,7 @@ class ViewApartmentView(DetailView):
         context = super().get_context_data(**kwargs)
         context['photos'] = Photo.objects.filter(apartment=context['apartment'])
         context['videos'] = Video.objects.filter(apartment=context['apartment'])
+        print(context['videos'])
         return context
 class AddApartmentView(LoginRequiredMixin, CreateView): 
     login_url = "/login/"
@@ -42,7 +50,7 @@ class AddApartmentView(LoginRequiredMixin, CreateView):
     
     
 
-class EditApartmentView(UpdateView, LoginRequiredMixin):
+class EditApartmentView(LoginRequiredMixin, UpdateView):
     login_url = "/login/"
     template_name = 'apartments/edit_apart.html'
     model = Apartment
@@ -53,7 +61,10 @@ class EditApartmentView(UpdateView, LoginRequiredMixin):
         "rent_price",
     ] 
     success_url ="/"
-
+    def form_valid(self, form):
+        instance = form.save(commit=False)
+        instance.user = self.request.user
+        super(EditApartmentView, self).save(form)
 
 def reverse_public_visibility(request, pk):
     apartment = Apartment.objects.get(pk=pk)
@@ -70,3 +81,49 @@ def delete_apartment(request, pk):
     response = HttpResponse()
     response["HX-Redirect"] = "/"
     return response
+
+def add_media_page(request, pk):
+    apartment = get_object_or_404(Apartment, pk=pk)
+    if apartment.owner != request.user:
+        return redirect('/')
+    videos = Video.objects.filter(apartment=apartment)
+    context = {
+        'photos': Photo.objects.filter(apartment=apartment),
+        'videos': videos,
+        'pk': pk
+    }
+    if videos:
+        print(videos[0])
+    return render(request, 'apartments/add_media.html', context)    
+
+
+def add_media(request, pk):
+    data = request.FILES 
+    apartment = get_object_or_404(Apartment, pk=pk)
+    if apartment.owner != request.user:
+        return redirect('/')
+
+    content_type = data.get('media').content_type
+
+    if 'image' in content_type:
+        photo = Photo.objects.create(apartment=apartment, photo=data.get('media'))
+        return render(request, 'apartments/components/htmx-add-photo.html', {'photo': photo})
+    elif 'video' in content_type:
+        video= Video.objects.create(apartment=apartment, video=data.get('media'))
+        return render(request, 'apartments/components/htmx-add-video.html', {'video': video})
+
+    
+
+def delete_media(request, pk):
+    
+    if request.htmx.target[0] == 'p':
+        photo = Photo.objects.get(pk=pk)
+        if request.user == photo.apartment.owner:
+            photo.delete()
+
+    elif request.htmx.target[0] == 'v':
+        video = Video.objects.get(pk=pk)
+        if request.user == photo.apartment.owner:
+            video.delete()
+
+    return HttpResponse('')
